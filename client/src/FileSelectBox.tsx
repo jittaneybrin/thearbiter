665c67742a5c8fb83fdc83ae44943c44c03b04b5
adamsaleh <adamsaleh797@gmail.com>
2024-04-21 20:05:14 -0400

    Revert "Revert "Added Warning message and sidebar scroll""
    This reverts commit 50e482e6e6e307a52d49264c2575de86f728fe99.

--- client/src/FileSelectBox.tsx
+++ client/src/FileSelectBox.tsx
@@ -1,7 +1,11 @@
-import { ToggleButton, ToggleButtonGroup } from "@mui/material";
+import {
+  SnackbarContent,
+  ToggleButton,
+  ToggleButtonGroup,
+  Popover,
+} from "@mui/material";
 import React, { useState, useEffect } from "react";
 import { PdfUpload } from "./PdfUpload";
-
 
 export interface game {
   name: string;
@@ -9,32 +13,33 @@ export interface game {
 }
 
 interface fileSelectBoxComponentProps {
-  setSelectedGame : React.Dispatch<React.SetStateAction<game | undefined>>;
+  setSelectedGame: React.Dispatch<React.SetStateAction<game | undefined>>;
 }
-// const supportedGames = [{game: "chess", index: "0"}, {game: "catan", index: "1"}]
 
-export function FileSelectBox(props : fileSelectBoxComponentProps) {
+export function FileSelectBox(props: fileSelectBoxComponentProps) {
   const [alignment, setAlignment] = React.useState("web");
   const [gameNameValue, setGameNameValue] = React.useState("");
   const [file, setFile] = useState<File | null>(null);
-  const [indexValue, setIndexValue] = React.useState("");
   const [toggleButtons, setToggleButtons] = useState<game[]>([]);
+  const [snackBarOpen, setSnackBarOpen] = useState(false);
 
   useEffect(() => {
     fetch("http://127.0.0.1:5000/getSupportedGames")
-      .then(response => response.json())
-      .then(json => {
+      .then((response) => response.json())
+      .then((json) => {
         console.log(json.games);
         setToggleButtons(json.games);
       })
-      .catch(error => console.error("Error fetching supported games:", error));
+      .catch((error) =>
+        console.error("Error fetching supported games:", error)
+      );
   }, []);
 
   const handleChange = (
     event: React.MouseEvent<HTMLElement>,
     newAlignment: string
   ) => {
-    setAlignment(newAlignment); 
+    setAlignment(newAlignment);
   };
 
   const modalStyle = {
@@ -49,28 +54,34 @@ export function FileSelectBox(props : fileSelectBoxComponentProps) {
     p: 4,
   };
 
-
   const handleSubmit = () => {
-    //send file here
-    if (file) {
-      const formData = new FormData();
-      formData.append("the_file", file);
-      fetch("http://127.0.0.1:5000/uploadPDF", {
-        method: "POST",
-        body: formData,
-      })
-        .then((res) => res.json())
-        .then((json) => {
-          let index = json.index;
-          // I don't know what's happening with textFieldValue, but it seems to be blank here.
-          // console.log(`Created game ${textFieldValue} with index ${index}`);
-          console.log(`Created index ${index}`);
-          const newToggleButtons = [...toggleButtons, {name: `${gameNameValue}`, index: `${index}`}];
-          setToggleButtons(newToggleButtons);
+    if (gameNameValue == "") {
+      setSnackBarOpen(true);
+    } else {
+      //send file here
+      if (file) {
+        const formData = new FormData();
+        formData.append("the_file", file);
+        fetch("http://127.0.0.1:5000/uploadPDF", {
+          method: "POST",
+          body: formData,
         })
-        .catch((error) => {
-          console.error("Error uploading file:", error);
-        });
+          .then((res) => res.json())
+          .then((json) => {
+            let index = json.index;
+            // I don't know what's happening with textFieldValue, but it seems to be blank here.
+            // console.log(`Created game ${textFieldValue} with index ${index}`);
+            console.log(`Created index ${index}`);
+            const newToggleButtons = [
+              ...toggleButtons,
+              { name: `${gameNameValue}`, index: `${index}` },
+            ];
+            setToggleButtons(newToggleButtons);
+          })
+          .catch((error) => {
+            console.error("Error uploading file:", error);
+          });
+      }
     }
   };
 
@@ -85,17 +96,24 @@ export function FileSelectBox(props : fileSelectBoxComponentProps) {
       onChange={handleChange}
       aria-label="Platform"
     >
-      {toggleButtons.length > 1 && toggleButtons.map((game) => (
-        <ToggleButton key={game.index} value={game.name} onClick={() => {
-          const selectedGame: game = {
-            name: game.name,
-            index: game.index
-          };
-          props.setSelectedGame(selectedGame);
-        }}>
-          {game.name ? game.name.charAt(0).toUpperCase() + game.name.slice(1) : ""}
-        </ToggleButton>
-      ))}
+      {toggleButtons.length > 1 &&
+        toggleButtons.map((game) => (
+          <ToggleButton
+            key={game.index}
+            value={game.name}
+            onClick={() => {
+              const selectedGame: game = {
+                name: game.name,
+                index: game.index,
+              };
+              props.setSelectedGame(selectedGame);
+            }}
+          >
+            {game.name
+              ? game.name.charAt(0).toUpperCase() + game.name.slice(1)
+              : ""}
+          </ToggleButton>
+        ))}
       <ToggleButton value="add-pdf">
         <PdfUpload
           handleSubmit={handleSubmit}
@@ -103,6 +121,16 @@ export function FileSelectBox(props : fileSelectBoxComponentProps) {
           setFile={setFile}
         />
       </ToggleButton>
+      <Popover
+        open={snackBarOpen}
+        onClose={() => {
+          setSnackBarOpen(false);
+        }}
+        anchorReference="anchorPosition"
+        anchorPosition={{ top: 20, left: 545 }}
+      >
+        <SnackbarContent message={"Please provide a Game Name with a PDF"} />
+      </Popover>
     </ToggleButtonGroup>
   );
 }
